59dd220f7a7f2c9e8725ff1ee40ce5ad99a1ad62
sgd <guzmandsalvador@gmail.com>
2023-01-04 13:06:56 -0600

    modified functions and arguments

--- SDM.py
+++ SDM.py
@@ -1,4 +1,8 @@
+#!/usr/bin/env python
+
+import argparse
 import geopandas as gpd  
+import logging as lg
 import matplotlib.pyplot as plt
 import numpy as np
 import os
@@ -13,97 +17,119 @@ import sys
 from sdm_functions import Random_Points_in_polygon, mask_raster, resulting_raster
 
 if __name__ == "__main__":
-    ### define parameters
-    locs_file = sys.argv[1] ### csv file with columns ["longitude" "latitude"]
-    margin = float(sys.argv[2]) ### margin around the points (units according to crs)
-    buffer_size = float(sys.argv[3]) ### buffer around the points to avoid seudo absenses (units according to crs)
-    variables_folder = sys.argv[4] ### folder with raster layers
-    crs = "epsg:4326"
-    
-    ##################################################
-    ##### START 
-    ##################################################
-    ### extract species coord
-    sp = pd.read_csv(locs_file).dropna(axis=0)    
-    sp["point"] = 1 ### presence
-    pres_points = gpd.GeoDataFrame(sp, crs=crs, geometry=gpd.points_from_xy(sp.longitude, sp.latitude))
-
-    ##################################################
-    ### list rasters layers
-    variables_files = os.listdir(variables_folder)
-    variables = {}
-    for key in variables_files:
-            variables[key] = variables_folder + key
-            
-    ##################################################
-    ##### create area limits 
-    limits = [(sp.longitude.min() - margin),
-            (sp.latitude.min()  - margin),
-            (sp.longitude.max() + margin),
-            (sp.latitude.max()  + margin)]
-
-    ### create pseudo absences
-    buf = pres_points.buffer(buffer_size)
-    buf = gpd.GeoDataFrame(crs=crs, geometry=buf.to_crs(crs))
-
-    extent = [[limits[0], limits[1]], 
-            [limits[0], limits[3]],
-            [limits[2], limits[3]],
-            [limits[2], limits[1]]]
-    extent_poly = gpd.GeoDataFrame(index=[0], crs=crs, geometry=[Polygon(extent)])
-    absences_poly = gpd.overlay(extent_poly, buf, how="difference")
-
-    ### random point whitin extent
-    absences_points = Random_Points_in_polygon(absences_poly, 1000, crs)
-
-    ### concat all points
-    all_points = pd.concat([pres_points[["point", "longitude", "latitude", "geometry"]],
-                            absences_points[["point", "longitude", "latitude", "geometry"]]])
-    coord_list = [(x,y) for x,y in zip(all_points["geometry"].x , all_points["geometry"].y)]
-
-    ##################################################
-    ### load each raster and extract values by each point and from all the layer
-    var_names = list(variables.keys())
-    raster_df = pd.DataFrame() 
-    
-    for var in var_names:
-        masked = mask_raster(variables[var], extent_poly, crs, "tmp.tif")
-        all_points[var] = [x[0] for x in masked.sample(coord_list)]
-        var_df = pd.DataFrame(np.array(masked.read()).reshape([1,-1]).T)
-        raster_df = pd.concat([raster_df, var_df], axis=1)
+        ### define logger
+        lg.basicConfig(filename='sdm.log', filemode='w', format='%(name)s - %(levelname)s - %(message)s')
+        logger = lg.getLogger('sdm')
+        logger.setLevel(lg.INFO)
         
-    ### split dataset into train and test
-    split_points = random.sample(range(len(all_points.index)), round(len(all_points.index)*0.25))
-    train_x = all_points.iloc[split_points, :].loc[:, var_names]
-    train_y = all_points.iloc[split_points, :].loc[:, "point"].to_list()
-    test_points = all_points.loc[~all_points.index.isin(split_points), :] 
-    test_x = all_points.loc[~all_points.index.isin(split_points), var_names] 
-    test_y = all_points.loc[~all_points.index.isin(split_points), "point"].to_list() 
+        stdout_handler = lg.StreamHandler(sys.stdout)
+        stdout_handler.setLevel(lg.INFO)
+        logger.addHandler(stdout_handler)
+        
+        ### define arguments 
+        parser = argparse.ArgumentParser(prog = 'RandomForestSDM', description = 'Program to perform an SDM using Random Forest')
+        parser.add_argument('-l', '--locs_file') ### csv file with columns ["longitude" "latitude"]
+        parser.add_argument('-m', '--margin_size')
+        parser.add_argument('-b', '--buffer_size')
+        parser.add_argument('-v', '--vars_folder')
+        args = parser.parse_args()
+        crs = "epsg:4326"
+        
+        ##################################################
+        ##### START 
+        ##################################################
+        ### extract species coord
+        logger.info(f'Reading localities...')
+        sp = pd.read_csv(args.locs_file).dropna(axis=0)    
+        sp["point"] = 1 ### presence
+        pres_points = gpd.GeoDataFrame(sp, crs=crs, geometry=gpd.points_from_xy(sp.longitude, sp.latitude))
+        logger.info(f'Loaded {sp.shape[0]} valid localities')
 
-    ### train model
-    rf_model = RandomForestRegressor(n_estimators = 1000, criterion = "absolute_error", max_depth = None,
-                                    oob_score = False, n_jobs = -1, bootstrap=True, random_state = 123)
-    rf_model.fit(train_x, train_y)
+        ##################################################
+        ### list rasters layers
+        logger.info(f'Reading variable files...')
+        variables_files = os.listdir(args.vars_folder)
+        variables = {}
+        for key in variables_files:
+                variables[key] = args.vars_folder + key
+        logger.info(f'Found {len(variables_files)} in variables folder')
+                
+        ##################################################
+        ##### create area limits 
+        logger.info(f'Defining area of interest...')
+        limits = [(sp.longitude.min() - float(args.margin_size)),
+                  (sp.latitude.min()  - float(args.margin_size)),
+                  (sp.longitude.max() + float(args.margin_size)),
+                  (sp.latitude.max()  + float(args.margin_size))]
 
-    ### predict test and compute rmse
-    test_prediction = rf_model.predict(X = test_x)
-    rmse = mean_squared_error(y_true = test_y, y_pred = test_prediction, squared = False)
-    print("rmse value = ", rmse)
-    
-    ### extrapolate prediction to complete rasters
-    raster_df.columns = var_names
-    raster_prediction = rf_model.predict(X = raster_df)
-    
-    ### proyect results
-    results_raster, results_transform = resulting_raster("tmp.tif", raster_prediction)
+        ### create pseudo absences
+        logger.info(f'Creating (pseudo)absences...')
+        buf = pres_points.geometry.buffer(float(args.buffer_size))
+        buf = gpd.GeoDataFrame(crs=crs, geometry=buf.to_crs(crs))
+        
+        extent = [[limits[0], limits[1]], 
+                [limits[0], limits[3]],
+                [limits[2], limits[3]],
+                [limits[2], limits[1]]]
+        extent_poly = gpd.GeoDataFrame(index=[0], crs=crs, geometry=[Polygon(extent)])
+        absences_poly = gpd.overlay(extent_poly, buf, how="difference")
+        ### random point whitin extent
+        absences_points = Random_Points_in_polygon(absences_poly, 1000, crs)
+        ### concat all points
+        all_points = pd.concat([pres_points[["point", "longitude", "latitude", "geometry"]],
+                                absences_points[["point", "longitude", "latitude", "geometry"]]])
+        coord_list = [(x,y) for x,y in zip(all_points["geometry"].x , all_points["geometry"].y)]
+        ##################################################
+        ### load each raster and extract values by each point and from all the layer
+        logger.info(f'Loading rasters...')
+        var_names = list(variables.keys())
+        raster_df = pd.DataFrame() 
+        
+        for var in var_names:
+                logger.info(f'Loading {var} file...')
+                masked = mask_raster(variables[var], extent_poly, crs, "tmp.tif")
+                all_points[var] = [x[0] for x in masked.sample(coord_list)]
+                var_df = pd.DataFrame(np.array(masked.read()).reshape([1,-1]).T)
+                raster_df = pd.concat([raster_df, var_df], axis=1)
+                
+        ### split dataset into train and test
+        logger.info(f'Spliting train and test datasets...')
+        split_points = random.sample(range(len(all_points.index)), round(len(all_points.index)*0.25))
+        train_x = all_points.iloc[split_points, :].loc[:, var_names]
+        train_y = all_points.iloc[split_points, :].loc[:, "point"].to_list()
+        # test_points = all_points.loc[~all_points.index.isin(split_points), :] 
+        test_x = all_points.loc[~all_points.index.isin(split_points), var_names] 
+        test_y = all_points.loc[~all_points.index.isin(split_points), "point"].to_list() 
 
-    ##################################################
-    ##### FIGURE
-    ##################################################
-    fig, ax = plt.subplots()
-    plt.xlim([limits[0], limits[2]])
-    plt.ylim([limits[1], limits[3]])
-    ax.set_aspect("equal")
-    show(results_raster, ax = ax, transform=results_transform, cmap="gray") 
-    pres_points.plot(ax = ax, color="green", edgecolor = "black")   
-    plt.show()
+        ### train model
+        logger.info(f'Training model...')
+        rf_model = RandomForestRegressor(n_estimators = 1000, criterion = "absolute_error", max_depth = None,
+                                        oob_score = False, n_jobs = -1, bootstrap=True, random_state = 123)
+        rf_model.fit(train_x, train_y)
+
+        ### predict test and compute rmse
+        logger.info(f'Testing model...')
+        test_prediction = rf_model.predict(X = test_x)
+        rmse = mean_squared_error(y_true = test_y, y_pred = test_prediction, squared = False)
+        logger.info(f'Model RMSE = {rmse}')
+        
+        
+        ### extrapolate prediction to complete rasters
+        logger.info(f'Expanding model to complete area of interest...')
+        raster_df.columns = var_names
+        raster_prediction = rf_model.predict(X = raster_df)
+        
+        ### proyect results
+        results_raster, results_transform = resulting_raster("tmp.tif", raster_prediction)
+
+        ##################################################
+        ##### FIGURE
+        ##################################################
+        logger.info(f'Ploting...')
+        fig, ax = plt.subplots()
+        plt.xlim([limits[0], limits[2]])
+        plt.ylim([limits[1], limits[3]])
+        ax.set_aspect("equal")
+        show(results_raster, ax = ax, transform=results_transform, cmap="gray") 
+        pres_points.plot(ax = ax, color="green", edgecolor = "black")   
+        plt.show()
--- sdm_functions.py
+++ sdm_functions.py
@@ -7,18 +7,39 @@ from rasterio.mask import mask
 
 def Random_Points_in_polygon(polygon, number, crs):   
     '''
-    modified from:
-    https://www.matecdev.com/posts/random-points-in-polygon.html
+    Generate random points whitin an extent.
+    
+    Modified from:https://www.matecdev.com/posts/random-points-in-polygon.html
+    
+    Args:
+        polygon: extension where the points will be generated
+        number: number of point to generate
+        crs: CRS of the resulting points
+    
+    Returns:
+        A geopandas dataframe with points geometries.
     '''
     df = pd.DataFrame()
     df["longitude"] = np.random.uniform( polygon.bounds["minx"], polygon.bounds["maxx"], number )
     df["latitude"] = np.random.uniform( polygon.bounds["miny"], polygon.bounds["maxy"], number )
     df["point"] = 0 ### absence
-    gdf_points = gpd.GeoDataFrame(df, crs=crs, geometry=gpd.points_from_xy(df["longitude"], df["latitude"]))
-    res_points = gpd.overlay(gdf_points, polygon, how="intersection")
-    return res_points
+    df = gpd.GeoDataFrame(df, crs=crs, geometry=gpd.points_from_xy(df["longitude"], df["latitude"]))
+    df = gpd.overlay(df, polygon, how="intersection")
+    return df
 
 def mask_raster(raster_path, mask_poly, crs, out_raster):
+    '''
+    Mask raster according to polygon.
+    
+    Args:
+        raster_path: path to input raster file
+        mask_poly: polygon to use as mask
+        crs: CRS of the result raster
+        out_raster: path to the resulting raster file
+    
+    Returns:
+        An masked raster
+    '''
     raster = rasterio.open(raster_path)
     out_img, out_transform  = mask(raster, mask_poly.geometry, invert=False, crop=True)
     new_raster = rasterio.open(out_raster, 'w', driver='GTiff',
@@ -32,8 +53,18 @@ def mask_raster(raster_path, mask_poly, crs, out_raster):
     return new_raster
 
 def resulting_raster(base_raster, new_vals):
-    raster= rasterio.open(base_raster)
-    results_raster = raster.read(1)
-    results_transform = raster.transform
+    '''
+    Create a new raste using another raster as base and a numpy array of data.
+    
+    Args:
+        base_raster: Raster file used as base for the new raster.
+        new_vals: A numpy array of values for the new raster.
+        
+    Returns:
+        A new raster with the attributes of the base raster and the values of the numpy array
+    '''
+    base_raster = rasterio.open(base_raster)
+    results_raster = base_raster.read(1)
+    results_transform = base_raster.transform
     results_raster = new_vals.reshape(results_raster.shape)
     return results_raster, results_transform
