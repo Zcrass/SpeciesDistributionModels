14f9e645e18cc34f0a9e21fbfeaf3975ef15cdc1
Zcrass <guzmandsalvador@gmail.com>
2023-03-08 18:51:49 -0600

    code complete. Needs optimization and text

--- sdm_functions.py
+++ sdm_functions.py
@@ -8,8 +8,16 @@ from shapely.geometry import Polygon
 
 
 class sdm_functions:
-    def list_rasters(folder):    
-        ### list rasters layers
+    def list_files(folder):   
+        '''
+        Creates a dictionary with the name and path of the files foun in a certain folder.
+        
+        Args:
+            folder: Folder to look for files.
+            
+        Returns:
+            A dictionay with the base names and paths of the files found in the folder.
+        ''' 
         variables_files = os.listdir(folder)
         variables = {}
         for key in variables_files:
@@ -24,9 +32,9 @@ class sdm_functions:
         Modified from:https://www.matecdev.com/posts/random-points-in-polygon.html
         
         Args:
-            polygon: extension where the points will be generated
-            number: number of point to generate
-            crs: CRS of the resulting points
+            polygon: extension where the points will be generated.
+            number: number of point to generate.
+            crs: CRS of the resulting points.
         
         Returns:
             A geopandas dataframe with points geometries.
@@ -39,18 +47,19 @@ class sdm_functions:
         df = gpd.overlay(df, polygon, how="intersection")
         return df
 
+
     def mask_raster(raster_path, mask_poly, crs, out_raster):
         '''
         Mask raster according to polygon.
         
         Args:
-            raster_path: path to input raster file
-            mask_poly: polygon to use as mask
-            crs: CRS of the result raster
-            out_raster: path to the resulting raster file
+            raster_path: path to input raster file.
+            mask_poly: polygon to use as mask.
+            crs: CRS of the result raster.
+            out_raster: path to the resulting raster file.
         
         Returns:
-            An masked raster
+            An masked raster.
         '''
         raster = rasterio.open(raster_path)
         out_img, out_transform  = mask(raster, mask_poly.geometry, invert=False, crop=True)
@@ -64,6 +73,7 @@ class sdm_functions:
         new_raster = rasterio.open(out_raster)
         return new_raster
 
+
     def resulting_raster(base_raster, new_vals):
         '''
         Create a new raste using another raster as base and a numpy array of data.
@@ -73,7 +83,7 @@ class sdm_functions:
             new_vals: A numpy array of values for the new raster.
             
         Returns:
-            A new raster with the attributes of the base raster and the values of the numpy array
+            A new raster with the attributes of the base raster and the values of the numpy array.
         '''
         base_raster = rasterio.open(base_raster)
         results_raster = base_raster.read(1)
@@ -81,9 +91,17 @@ class sdm_functions:
         results_raster = new_vals.reshape(results_raster.shape)
         return results_raster, results_transform
     
+
     def extent_poly(df, margin_size, crs):
         '''
         Creates a geopandas dataframe based on the extenssion of an input dataframe and a defined margin.
+        Args:
+            df: Aandas dataframe with decimalLongitude and decimalLatitude columns.
+            margin_size: Margin drawn around the points. Values is based on the CRS used.
+            crs: CRS to use in the geopandas dataframe.
+            
+        Returns:
+            A new raster with the attributes of the base raster and the values of the numpy array.
         '''
         extent = [[(df.decimalLongitude.min() - float(margin_size)),
                         (df.decimalLatitude.min()  - float(margin_size))],
@@ -95,5 +113,19 @@ class sdm_functions:
                         (df.decimalLatitude.min()  - float(margin_size))]
                  ]
         extent = gpd.GeoDataFrame(index=[0], crs=crs, geometry=[Polygon(extent)])
-
         return extent
+
+
+    def cor_test(df, thr):
+        '''
+        Computes the correlation matrix and extract the name of the correlated variables.
+        Args:
+            df: dataframe to compute correlation.
+            thr: threshold to use as cut off to define the high correlation.
+            
+        Returns:
+            A list of the correlated variables.
+        '''    
+        cor_mat = df.corr().abs()
+        cor_mat = cor_mat.where(np.triu(np.ones(cor_mat.shape),k=1).astype(bool))
+        return [column for column in cor_mat.columns if any(cor_mat[column] > thr)]
